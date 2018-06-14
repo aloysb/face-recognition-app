763e3aad260253df46fde372cdb2888479af49b1
Aloys Berger <aloysberger@gmail.com>
2018-06-14 19:38:37 +0200

    Linked to server

--- src/App.js
+++ src/App.js
@@ -34,8 +34,26 @@ class App extends Component {
       imageUrl:'',
       box:'',
       route:'signin',
-      isSignedIn:false
+      isSignedIn:false,
+      user: {
+        id:'' ,
+        name: '',
+        email: '',
+        entries:0,
+        joined:''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState(
+    {user:{
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined  
+    }})
   }
 
   calculateFaceLocation=(data)=>{
@@ -43,7 +61,6 @@ class App extends Component {
     const image=document.getElementById('inputimage');
     const width=Number(image.width);
     const height=Number(image.height);
-    console.log('height:',height,' . widht: ',width);
     return{
       leftCol: clarifaiFace.left_col*width,
       topRow: clarifaiFace.top_row*height,
@@ -53,7 +70,6 @@ class App extends Component {
   }
 
   displayFaceBox=(box)=>{
-    console.log(box);
     this.setState({box:box});
   }
 
@@ -62,11 +78,29 @@ class App extends Component {
   }
 
   onButtonSubmit=()=>{
+
     this.setState({imageUrl:this.state.input})
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL,
       this.state.input)
-    .then(response=>this.displayFaceBox(this.calculateFaceLocation(response)))
+    .then(response=>{
+
+      if(response){
+        fetch(('http://localhost:3000/image'),{
+          method: 'put',
+          headers: {'content-type':'application/json'},
+          body: JSON.stringify({
+            id:this.state.user.id
+          })
+        })
+        .then(resp => resp.json())
+        .then(count=> {
+          this.setState(Object.assign(this.state.user,{entries:count}))
+        })
+       }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
+
     .catch(err=>console.log(err));
   }
 
@@ -91,7 +125,7 @@ class App extends Component {
         {route === 'home'
           ? <div>
               <Logo />
-              <Rank />
+              <Rank name={this.state.user.name} entries={this.state.user.entries}/>
               <ImageLinkForm 
                 onInputChange={this.onInputChange} 
                 onButtonSubmit={this.onButtonSubmit}
@@ -100,8 +134,8 @@ class App extends Component {
             </div>
           : (
             route === 'signin' 
-            ? <SignIn onRouteChange={this.onRouteChange}/>
-            : <Register onRouteChange={this.onRouteChange}/>
+            ? <SignIn loadUser = {this.loadUser} onRouteChange={this.onRouteChange}/>
+            : <Register loadUser= {this.loadUser} onRouteChange={this.onRouteChange}/>
             )
         }
       </div>
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,6 +1,44 @@
 import React from 'react';
 
-const SignIn=({ onRouteChange })=>{
+class SignIn extends React.Component {
+
+	constructor(props){
+		super(props);
+		this.state={
+			signInEmail:'',
+			signInPassword:''
+		}
+	}
+
+	onEmailChange = (event) => {
+		this.setState({signInEmail: event.target.value})
+	}
+
+	onPasswordChange = (event) => {
+		this.setState({signInPassword: event.target.value})
+	}
+
+	onSubmitSignIn = () => {
+		fetch('http://localhost:3000/signin',{
+			method:'post',
+			headers: {'Content-type': 'application/json'},
+			body: JSON.stringify({
+				email: this.state.signInEmail,
+				password: this.state.signInPassword
+			})
+		})
+			.then(response => response.json())
+			.then(user =>{
+				if (user.id){
+					this.props.loadUser(user);
+					this.props.onRouteChange('home');
+				}
+			})
+	}
+
+	render(){
+	const {onRouteChange}=this.props;
+
 	return(
 		<article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
 			<main className="pa4 black-80">
@@ -9,16 +47,28 @@ const SignIn=({ onRouteChange })=>{
 			      <legend className="f1 fw6 ph0 mh0">Sign In</legend>
 			      <div className="mt3">
 			        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-			        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
+			        <input 
+			        	onChange={this.onEmailChange}
+			        	className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+			        	type="email" 
+			        	name="email-address"  
+			        	id="email-address"
+			        />
 			      </div>
 			      <div className="mv3">
 			        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-			        <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
+			        <input 
+			        	onChange={this.onPasswordChange}
+				        className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+				        type="password" 
+				        name="password"  
+				        id="password"
+				       />
 			      </div>
 			    </fieldset>
 			    <div className="">
 			      <input 
-			      	onClick={()=>onRouteChange('home')}
+			      	onClick={this.onSubmitSignIn}
 			      	className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
 			      	type="submit" 
 			      	value="Sign in"
@@ -30,7 +80,8 @@ const SignIn=({ onRouteChange })=>{
 	  			</div>
 			</main>
 		</article>
-	)
+		)
+	}
 }
 
 export default SignIn;
